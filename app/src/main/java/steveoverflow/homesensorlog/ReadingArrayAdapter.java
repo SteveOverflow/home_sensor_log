e3b37e1d0ca3a491ddebcca9cabf759941f73e08
Stephen Tanton <stephen.tanton@gmail.com>
2015-07-19 20:05:40 -0300

    Refines list item view of previous readings.

--- app/src/main/java/steveoverflow/homesensorlog/ReadingArrayAdapter.java
+++ app/src/main/java/steveoverflow/homesensorlog/ReadingArrayAdapter.java
@@ -2,6 +2,7 @@ package steveoverflow.homesensorlog;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,7 @@ import com.android.volley.toolbox.NetworkImageView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -37,15 +39,30 @@ public class ReadingArrayAdapter extends ArrayAdapter<JSONObject> {
     public View getView(int position, View convertView, ViewGroup parent){
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.reading_list_item, parent, false);
-        TextView textView = (TextView) rowView.findViewById(R.id.readingText);
+        TextView dateText = (TextView) rowView.findViewById(R.id.readingDate);
+        TextView readingLine1 = (TextView) rowView.findViewById(R.id.readingLine1);
+        TextView readingLine2 = (TextView) rowView.findViewById(R.id.readingLine2);
         NetworkImageView imageView = (NetworkImageView) rowView.findViewById(R.id.readingImage);
 
         try{
             JSONObject reading = values.get(position);
             int lightVal = reading.getInt("lightVal");
             String img = ReadingsUtil.evaluateLightLevel(lightVal);
-            imageView.setImageUrl(res.getString(R.string.baseURL) + "/images/" + img +".png", mImageLoader);
-            textView.setText(reading.toString());
+            imageView.setImageUrl(res.getString(R.string.baseURL) + "/images/" + img + ".png", mImageLoader);
+
+            String dateString = reading.getString("date");
+            Log.d("DATE",dateString);
+            Date readingDate = DateFormatter.parseJavascriptDate(dateString);
+            dateText.setText(DateFormatter.formatDisplayDate(readingDate));
+
+            String line1 = "", line2 = "";
+
+            line1 += "Temperature: " + reading.getString("tempVal") + (char) 0x00B0 + "C  Humidity: " + reading.getString("humidityVal") + "%";
+            readingLine1.setText(line1);
+
+            Long pressureVal = reading.getLong("pressureVal");
+            line2 += "Pressure: "+(pressureVal / 10.0) +" kPa";
+            readingLine2.setText(line2);
         }catch(JSONException je){
             je.printStackTrace();
         }
